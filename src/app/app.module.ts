539c79ffb6902d1d84c7597483faca1d3ac30c21
Aaron Bae <aaronbae@uw.edu>
2019-02-18 23:34:28 -0800

    Can't get the image to load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    NavigationbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
