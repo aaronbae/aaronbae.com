d8955320ce44c5f329e5d974660acd6e770109a6
Aaron Bae <aaronbae@uw.edu>
2019-06-08 21:47:08 -0700

    started django project instead

--- src/app/_models/User.ts
+++
@@ -1,9 +0,0 @@
-// User.ts
-
-export default class User {
-    _id: Number;
-    login_email: String;
-    login_password: String;
-    first_name: String;
-    last_name: String;
-  }
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  mongoUrl: 'http://cluster0-ecnt3.mongodb.net'
-};
