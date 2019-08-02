94d6f2faa6f23f57bab668dc370806cf7314e1cb
Aaron Bae <aaronbae@uw.edu>
2019-08-01 20:33:28 -0700

    finished routing for the landing page

--- frontend/src/Common/Header.js
+++ frontend/src/Common/Header.js
@@ -1,44 +1,44 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import './Header.css';
+import './Header.scss';
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: 0
-    };
+      visible: true,
+      prevScroll: 0
+    }
     this.handleScroll = this.handleScroll.bind(this);
   }
+
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+     window.removeEventListener('scroll', this.handleScroll);
   }
-  
+
   handleScroll() {
-    var anchors = document.getElementsByClassName('nav-anchor');
-    
-    for (var i = anchors.length - 1; i >=0; i--) {
-      if (anchors[i].getBoundingClientRect().top <= 0 ) {
-        this.setState({active: i});
-        break;
-      }
-    }
+    const currScroll = window.scrollY;
+    const currVisible = this.state.prevScroll - currScroll >= -0;
+    //console.log(this.state.prevScroll - currScroll);
+    this.setState({
+      visible: currVisible,
+      prevScroll: currScroll
+    });
   }
 
   render() {
+    const path = window.location.pathname
     return (
-      <Navbar className="row" sticky="top" bg="light" variant="light">
+      <Navbar fixed="top" className={this.state.visible ? "row": "row hidden"}>
         <Navbar.Brand href="/">Aaron Bae</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home" className={this.state.active===0? "active": ""} >Home</Nav.Link>
-          <Nav.Link href="#projects" className={this.state.active===1? "active": ""} >Projects</Nav.Link>
-          <Nav.Link href="#resume" className={this.state.active===2? "active": ""} >Resume</Nav.Link>
-          <Nav.Link href="#blog" className={this.state.active===3? "active": ""} >Blog</Nav.Link>
-        </Nav>
+        <div className='mr-auto'>
+          <Nav.Link className={path==="/"?"navtab active":"navtab"} href="/">Home</Nav.Link>
+          <Nav.Link className={path==="/blog"?"navtab active":"navtab"} href="/blog">Blog</Nav.Link>
+        </div>
       </Navbar>
     );
   }
--- frontend/src/Common/Main.js
+++ frontend/src/Common/Main.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import './Main.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './Header';
-import Home from './Home';
-import Projects from './Projects';
+import Footer from './Footer';
+import Home from '../Home/Home';
+import Blog from '../Blog/Blog';
 
 class Main extends Component {
   render() {
     return (
-      <div className='container-fluid'>
-        <Header />
-        <Home />
-        <Projects />
-      </div>
+      <Router>
+        <div className='container-fluid base_container'>
+          <Header />
+          
+          <Route exact path="/" component={Home} />
+          <Route path="/blog" component={Blog} />
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
