d93589edbe8e0bf7900259ba21ca392aae37147d
Aaron Bae <aaronbae@uw.edu>
2019-10-26 14:57:52 -0700

    white lines again

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB');
 
+
 // Configuring different routes
 const defaultRoute = require('./routes/Default.route');
 const postRoute = require('./routes/Post.route');
