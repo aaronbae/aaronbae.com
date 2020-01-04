9885e5668c74b1bd0aa95171384ac2d420646eb8
Aaron Bae <aaronbae@uw.edu>
2020-01-04 00:58:29 -0800

    finished non-git-route. will change back

--- frontend/src/Common/Header.js
+++ frontend/src/Common/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import './Header.scss';
@@ -7,36 +6,7 @@ import './Header.scss';
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      visible: true,
-      prevScroll: 0
-    }
-    this.handleScroll = this.handleScroll.bind(this);
     this.handleLogOut = this.handleLogOut.bind(this);
-    this.handleLogIn = this.handleLogIn.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    const currScroll = window.scrollY;
-    const currVisible = this.state.prevScroll - currScroll >= -0;
-    //console.log(this.state.prevScroll - currScroll);
-    this.setState({
-      visible: currVisible,
-      prevScroll: currScroll
-    });
-  }
-
-  handleLogIn(e) {
-    e.stopPropagation();
-    console.log("Shit in")
   }
 
   handleLogOut(e) {
@@ -48,20 +18,21 @@ class Header extends Component {
     const { logged_in } = this.props;
     const path = window.location.pathname
     return (
-      <Navbar fixed="top" className={this.state.visible ? "row": "row hidden"}>
-        <Navbar.Brand href="/">Aaron Bae</Navbar.Brand>
-        <div className='mr-auto'>
-          <Nav.Link className={path==="/"?"navtab active":"navtab"} href="/">Home</Nav.Link>
-          <Nav.Link className={path==="/blog"?"navtab active":"navtab"} href="/blog">Blog</Nav.Link>  
-          {logged_in && 
-            <Nav.Link className="navtab" href="/login">Sign Out</Nav.Link>
-          }
-          {!logged_in &&
-            <Nav.Link className="navtab" href="/login">Sign In</Nav.Link>
-          }
-            
+      <div className="header-container row">
+        <div><a className="navbrand" href="/">Aaron Bae</a></div>
+        <div><a className={path==="/"?"navlink active":"navlink "} href="/">Home</a></div>
+        <div className="line"></div>
+        <div><a className={path==="/blog"?"navlink active":"navlink"} href="/blog">Blog</a></div>
+        <div className="line"></div>
+        <div className="float-right">
+        {logged_in && 
+          <button className={path==="/login"?"navlink sign-out-button active":"navlink sign-out-button"} onClick={this.handleLogOut}>Sign Out</button>
+        }
+        {!logged_in &&
+          <a className={path==="/login"?"navlink active":"navlink"} href="/login">Admin</a>
+        }
         </div>
-      </Navbar>
+      </div>
     );
   }
 }
