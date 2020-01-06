dad268ca897e69bbb27c8daaca0958d11683c872
Aaron Bae <aaronbae@uw.edu>
2020-01-06 13:42:57 -0800

    changed the production constants for frontend

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,7 +1,7 @@
 const prod = {
   url: {
-    POST_URL: "http://localhost:4000/posts/",
-    USER_URL: "http://localhost:4000/users/",
+    POST_URL: "http://ec2-54-188-201-9.us-west-2.compute.amazonaws.com/api/posts/",
+    USER_URL: "http://ec2-54-188-201-9.us-west-2.compute.amazonaws.com/api/users/",
   }
 }
 const dev = {
