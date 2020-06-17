0ae7326909e1a3fcb2e49c651dc6c98159aff10e
Aaron Bae <aaron@aaronbae.com>
2020-06-17 16:39:25 -0700

    fixed a few style issues

--- frontend/src/Common/Header.js
+++ frontend/src/Common/Header.js
@@ -31,45 +31,49 @@ class Header extends Component {
     const path = this.props.history.location.pathname;
     return (
       <div className="header-container d-flex row">
-        <div><RedirectButton addClasses="navbrand" message="Aaron Bae" route="/" /></div>
+        <div className="offset-md-2 col-12 col-md-8 header-inner-container">
+          <div className="row">
+            <div><RedirectButton addClasses="navbrand" message="Aaron Bae" route="/" /></div>
 
-        <div className="d-none d-sm-block collapsable">
-          <div className={path==="/"?"tab active":"tab"}>
-            <RedirectButton addClasses="navlink" message="Home" route="/" />
-          </div>
+            <div className="d-none d-sm-block collapsable">
+              <div className={path==="/"?"tab active":"tab"}>
+                <RedirectButton addClasses="navlink" message="Home" route="/" />
+              </div>
 
-          <div className="tab line"></div>
-          <div className={path.startsWith("/blog") || path.startsWith("/admin")?"tab active":"tab"}>
-            {logged_in && 
-              <RedirectButton addClasses="navlink" message="Blog" route="/admin" />
-            }
-            {!logged_in &&
-              <RedirectButton addClasses="navlink" message="Blog" route="/blog" />
-            }
-          </div>
+              <div className="tab line"></div>
+              <div className={path.startsWith("/blog") || path.startsWith("/admin")?"tab active":"tab"}>
+                {logged_in && 
+                  <RedirectButton addClasses="navlink" message="Blog" route="/admin" />
+                }
+                {!logged_in &&
+                  <RedirectButton addClasses="navlink" message="Blog" route="/blog" />
+                }
+              </div>
 
-          <div className={path==="/login"?"tab admin-container active":"admin-container"}>
-            {logged_in && 
-              <button className="navlink adminButton" onClick={this.handleLogOut}>Sign Out</button>
-            }
-            {!logged_in &&
-              <RedirectButton addClasses="navlink adminButton" message="Admin" route="/login" />
-            }
+              <div className={path==="/login"?"tab admin-container active":"admin-container"}>
+                {logged_in && 
+                  <button className="navlink adminButton" onClick={this.handleLogOut}>Sign Out</button>
+                }
+                {!logged_in &&
+                  <RedirectButton addClasses="navlink adminButton" message="Admin" route="/login" />
+                }
+              </div>
+            </div>
+            <div className="d-block d-sm-none dropdown-container">
+              <div className="dropdown">
+                <button className="btn" type="button" data-toggle="dropdown">
+                  <img className='menu-icon' src="assets/icons/hamburger_icon.png" alt="menu" />
+                </button>
+                <div className="dropdown-menu dropdown-menu-right" >
+                  <RedirectButton addClasses="dropdown-item" message="Home" route="/" />
+                  <RedirectButton addClasses="dropdown-item" message="Blog" route="/blog" />
+                  <div className="dropdown-divider"></div>
+                  <RedirectButton addClasses="dropdown-item" message="Admin" route="/login" />
+                </div>
+              </div>
+            </div>  
           </div>
         </div>
-        <div className="d-block d-sm-none dropdown-container">
-          <div className="dropdown">
-            <button className="btn" type="button" data-toggle="dropdown">
-              <img className='menu-icon' src="assets/icons/hamburger_icon.png" alt="menu" />
-            </button>
-            <div className="dropdown-menu dropdown-menu-right" >
-              <RedirectButton addClasses="dropdown-item" message="Home" route="/" />
-              <RedirectButton addClasses="dropdown-item" message="Blog" route="/blog" />
-              <div className="dropdown-divider"></div>
-              <RedirectButton addClasses="dropdown-item" message="Admin" route="/login" />
-            </div>
-          </div>
-        </div>  
       </div>
     );
   }
