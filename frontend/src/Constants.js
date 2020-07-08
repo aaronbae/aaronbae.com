6b13e74adeaa704c1dbca6433e5ec6bd8bc509d5
Aaron Bae <aaron@aaronbae.com>
2020-07-08 00:39:41 -0700

    created a constant for file upload url

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,11 +1,13 @@
 const prod = {
   url: {
+    FILE_URL: "https://aaronbae.com/api/files/",
     POST_URL: "https://aaronbae.com/api/posts/",
     USER_URL: "https://aaronbae.com/api/users/",
   }
 }
 const dev = {
   url: {
+    FILE_URL: "http://localhost:4000/files/",
     POST_URL: "http://localhost:4000/posts/",
     USER_URL: "http://localhost:4000/users/",
   }
