2030213b20c999d023ec5cbb384fc650619f7d1f
Aaron Bae <aaron@aaronbae.com>
2020-07-19 03:34:13 -0700

    implemented helmet

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -2,15 +2,14 @@ import path from 'path';
 import fs from 'fs';
 
 import express from 'express';
-
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import { renderToString } from 'react-dom/server'
-
 import { Provider } from 'react-redux';
 import store from '../src/Redux/Store';
-
 import Main from '../src/Common/Main';
+import DocumentMeta  from 'react-document-meta';
+import Helmet from 'react-helmet';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -28,15 +27,18 @@ app.get('*', (req, res) => {
     </StaticRouter>
   );
   console.log("Serving out " + req.url)
+  let helmet = Helmet.renderStatic();
+  let meta = `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}`
+
   const indexFile = path.resolve('./build/index.html');
   fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
       console.error('Something went wrong:', err);
       return res.status(500).send('Oops, better luck next time!');
     }
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${main}</div>`)
-    );
+    data = data.replace('<div id="root"></div>', `<div id="root">${main}</div>`)
+    data = data.replace('<meta react-meta-document>', `${meta}`)
+    return res.send(data);
   });
   
 });
