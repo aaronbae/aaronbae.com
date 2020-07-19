12d2436bb7dc2aa7cc714b43d3cd31f237fef264
Aaron Bae <aaron@aaronbae.com>
2020-07-18 22:56:04 -0700

    combined redux stores

---
+++ frontend/src/Redux/Store.js
@@ -0,0 +1,12 @@
+
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import BlogReducer from './BlogReducer';
+import AdminReducer from './AdminReducer';
+
+const rootReducer = combineReducers({
+  BlogReducer,
+  AdminReducer
+});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export default store
