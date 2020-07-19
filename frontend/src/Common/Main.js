43a3efd78673026cab2d79da81d91a6677548eac
Aaron Bae <aaron@aaronbae.com>
2020-07-18 23:20:26 -0700

    reordered imports

--- frontend/src/Common/Main.js
+++ frontend/src/Common/Main.js
@@ -1,11 +1,9 @@
-// Bootstrap imports
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 // React Engine Imports
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
+import { Provider } from 'react-redux';
+import DocumentMeta from 'react-document-meta';
 
-// Components
 import Header from './Header';
 import Footer from './Footer';
 import Home from '../Home/Home';
@@ -18,16 +16,10 @@ import Testing from '../Testing/Main';
 import NonExistentRoute from '../Warnings/NonExistentRoute';
 import './Main.scss';
 
-// Redux 
-import { Provider } from 'react-redux';
+// Other 
 import store from "../Redux/Store";
-
-// Dynamic Meta tag manager
-import DocumentMeta from 'react-document-meta';
-
-
-// Sentry
 import * as Sentry from '@sentry/react';
+
 Sentry.init({dsn: "https://eafc03ce34b94dd98b03f1fc08903681@o418535.ingest.sentry.io/5321569"});
 
 class Main extends Component {
