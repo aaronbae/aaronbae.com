64f043bdbaa23d4c22e72cb9c9729e93fe291e95
Aaron Bae <aaron@aaronbae.com>
2020-07-18 23:51:16 -0700

    built

---
+++ frontend/build/precache-manifest.58be086daebdd9642e779f1132badfe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf874fb0c793fbb35c88f8e1168f2ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eda887e775320b862408",
+    "url": "/static/css/main.d471121b.chunk.css"
+  },
+  {
+    "revision": "4a759c42c5121320ab93",
+    "url": "/static/js/2.808624e7.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.808624e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda887e775320b862408",
+    "url": "/static/js/main.1f84847b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
