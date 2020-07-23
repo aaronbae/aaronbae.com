46039d7d9eea66c2b086d68816bc9f172fb71cc1
Aaron Bae <aaron@aaronbae.com>
2020-07-22 19:44:30 -0700

    meta editor finished

--- server/routes/Default.route.js
+++ server/routes/Default.route.js
@@ -11,23 +11,27 @@ import store from '../../src/Redux/Store';
 import Main from '../../src/Common/Main';
 import Helmet from 'react-helmet';
 import Post from '../models/Post';
-
+import { metaFromPost } from "../../src/Utils/MetaHelpers";
 
 const express = require('express');
 const defaultRoutes = express.Router();
 
-function serveFrontend(req, res) {
-  const main = renderToString(
+function serveFrontend(req, res, post) {
+  // Render component
+  let main = renderToString(
     <StaticRouter location={req.url}>
       <Provider store={store}>
+        {("title" in post && "content" in post && "_id" in post) && metaFromPost(post)}
         <Main />
       </Provider>
     </StaticRouter>
   );
-  const finalState = store.getState()
+
+  // Manage Meta Information
   let helmet = Helmet.renderStatic();
   let meta = `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}`
 
+  // Read index.html and send
   const indexFile = path.resolve('./build/index.html');
   fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
@@ -37,7 +41,6 @@ function serveFrontend(req, res) {
     }
     data = data.replace('<div id="root"></div>', `<div id="root">${main}</div>`)
     data = data.replace('<meta react-meta-document>', `${meta}`)
-    data = data.replace('__REDUX_PRELOAD__', `${JSON.stringify(finalState)}`)
     console.log(`${req.url} : Successfully served the page!`)
     return res.send(data);
   });
@@ -51,13 +54,13 @@ defaultRoutes.route("/blog/[a-zA-Z0-9]+").get(function (req, res) {
       console.log(err);
     }
     else {
-      serveFrontend(req, res);
+      serveFrontend(req, res, post);
     }
   });
 });
 
 defaultRoutes.route("/*").get(function (req, res) {
-  serveFrontend(req, res);
+  serveFrontend(req, res, {});
 });
 
 export default defaultRoutes;
