34466d4e0d3d3fe7b06fbee5112edaffcc3a0332
Aaron Bae <aaron@aaronbae.com>
2020-08-14 00:48:16 -0700

    finished header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -8,24 +9,46 @@ import '../styles/header.css'
 export default function Header() {
   const dispatch = useDispatch()
   const router = useRouter()
+  const dropdown = useRef(null)
   const logged_in = useSelector((state) => state.AdminReducer.logged_in)
-  const handle_sign_out = (e) => {
+  const hide_n_stop = (e)=> {
+    //hide()
+    console.log(document.activeElement.tagName)
+  }
+  const hide = () => {
+    dropdown.current.classList.add("hide")
+  }
+  const toggle_dropdown = () => {
+    const e = dropdown.current
+    if(e.classList.contains("hide")){
+      e.classList.remove("hide")
+    } else {
+      e.classList.add("hide")
+    }
+  }
+  const handle_sign_out = () => {
+    hide()
     dispatch(sign_out())
     router.push("/login")
   }
   return (
     <header className="header">
-      <Link href="/"><a className="logo">Aaron Bae</a></Link>
-      <nav className="tab-group">
-        <Link href="/"><a className="tab">Home</a></Link>
-        <Link href="/blog"><a className="tab">Blog</a></Link>
+      <Link href="/"><a className="logo" onClick={hide}>Aaron Bae</a></Link>
+      <button className="hamburger" onClick={toggle_dropdown}>
+        <img src="/icons/hamburger_icon.png" />
+      </button>
+      <nav className="navigation hide" ref={dropdown}>
+        <div className="tab-group">
+          <Link href="/"><a className="tab" onClick={hide}>Home</a></Link>
+          <Link href="/blog"><a className="tab" onClick={hide}>Blog</a></Link>
+        </div>
+        {logged_in &&
+          <a className="tab admin-tab" onClick={handle_sign_out}>Sign Out</a>
+        }
+        {!logged_in && 
+          <Link href="/login"><a className="tab admin-tab" onClick={hide}>Admin</a></Link>
+        }
       </nav>
-      {logged_in &&
-        <a className="tab admin-tab" onClick={handle_sign_out}>Sign Out</a>
-      }
-      {!logged_in && 
-        <Link href="/login"><a className="tab admin-tab">Admin</a></Link>
-      }
     </header>
   )
 }
