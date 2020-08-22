f68387bd36c351c77c43aee498a66fc24acd6e03
Aaron Bae <aaron@aaronbae.com>
2020-08-21 18:36:08 -0700

    static information finished

---
+++ frontend/src/components/Projects/Project.js
@@ -0,0 +1,31 @@
+import Mapper from "./UrlMapper"
+import '../../styles/Projects/project.css'
+
+export default function Project(props) {
+  return (
+    <div className="project-container card-wrapper">
+      {props.section_title && 
+        <p className="card-title project-section-header">{props.section_title}</p> 
+      }
+      <div className="project-header">
+        <div className="project-title">
+          {props.title}
+        </div>
+        <div className="icon-wrapper">  
+          <a href={props.github_url} target="_blank" className="github-icon" title="Visit GitHub Repo for this Project">
+            <img src="icons/github.png" />
+          </a>
+          {props.private && <img src="icons/lock-icon.svg" className="lock-icon" title="This is a Private Project" alt="private project"/>}
+        </div>
+      </div>
+      <div className="project-content">
+        {props.children}
+      </div>
+      <div className="tech-tags">
+        {props.tech_tags.split(",").map((item, index)=>
+          <a key={item} href={Mapper[item.trim()]} target="_blank" >{item}</a>
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/components/Projects/UrlMapper.js
@@ -0,0 +1,12 @@
+const Mapper = {
+  NextJS: "https://nextjs.org/",
+  Node: "https://nodejs.org/en/",
+  Express: "https://expressjs.com/",
+  MongoDB: "https://www.mongodb.com/",
+  YahooAPI: "https://finance.yahoo.com/",
+  Python: "https://www.python.org/",
+  HuggingFace: "https://huggingface.co/", 
+  Pytorch: "https://pytorch.org/", 
+  HotpotQA: "https://hotpotqa.github.io/" 
+}
+export default Mapper
