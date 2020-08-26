24515455f19c752e724dc3700e6d54226224c26c
Aaron Bae <aaron@aaronbae.com>
2020-08-25 23:43:02 -0700

    switching to aws mongo

--- backend/DB.js
+++ backend/DB.js
@@ -1,4 +1,4 @@
-const prod = {
+const remote_mongo = {
   DB: "mongodb://aaronbae:kingoftheworld123@cluster0-shard-00-01-ecnt3.mongodb.net:27017,cluster0-shard-00-01-ecnt3.mongodb.net:27017,cluster0-shard-00-02-ecnt3.mongodb.net:27017?replicaSet=Cluster0-shard-0",
   options: {
     ssl: true,
@@ -10,7 +10,7 @@ const prod = {
   }
 };
 
-const dev = {
+const aws_mongo = {
   DB: 'mongodb://localhost:27017/aaronbaeDB',
   options: {
     useNewUrlParser: true,
@@ -18,4 +18,5 @@ const dev = {
   },
 };
 
-module.exports = process.env.NODE_ENV === "development" ? dev : prod;
+//module.exports = process.env.NODE_ENV === "development" ? dev : prod;
+module.exports = aws_mongo;
