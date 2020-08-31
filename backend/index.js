d66ff76a16fd550fabaed56474fd762b50f45aed
Aaron Bae <aaron@aaronbae.com>
2020-08-30 18:22:50 -0700

    trying new time

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.use('/api/stocks', stockRoute);
 app.use('/api/monitor', monitorRoute);
 
 // Cron Jobs
-cron.schedule('0 22 12 * * *', () => {
+cron.schedule('0 24 11 * * *', () => {
   // At Midnight
   cron_utils.reload_stocks()
 });
