2f25f5956835df2a24d8c588c97d290b05d41c13
Aaron Bae <aaron@aaronbae.com>
2020-09-02 00:56:06 -0700

    changed time for cron

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.use('/api/stocks', stockRoute);
 app.use('/api/monitor', monitorRoute);
 
 // Cron Jobs
-cron.schedule('0 0 7 * * *', () => {
+cron.schedule('0 59 7 * * *', () => {
   // At Midnight PST
   cron_utils.fetch_fresh_data()
 });
